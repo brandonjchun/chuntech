e95a253cf290d988cc4f786d7e41833cd9d05485
Brandon Chun <zeonbravo@gmail.com>
2021-12-03 02:31:44 -0800

    skeleton structure with navbar and pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import Contact from './Contact';
+import About from './About';
+import { Route, Routes, Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
+
 
 export default App;
